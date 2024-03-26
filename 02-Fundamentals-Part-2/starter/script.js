cd35ecf46f41ea12738f1c9aea8ea7ad0927dc9b
janagyjr <jnagyjr1978@gmail.com>
2024-03-26 15:17:34 -0500

    Next is functions!

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -4,5 +4,8 @@
 let hasDriversLicense = false;
 const passTest = true;
 
-if (passTest) hasDriverLicense = true;
+if (passTest) hasDriversLicense = true;
 if (hasDriversLicense) console.log("I can drive");
+
+// const interface = "Audio";
+// const private = 534;

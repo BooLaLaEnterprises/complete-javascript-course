686f2c3c762edbb0709ac998b566a7a5927b1655
janagyjr <jnagyjr1978@gmail.com>
2024-04-01 13:15:42 -0500

    more functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -61,9 +61,14 @@ console.log(yearsUntilRetirement(1991, "Jonas"));
 console.log(yearsUntilRetirement(1980, "Bob"));
 */
 
-function cutFruitPieces(fruit) {}
+function cutPieces(fruit) {
+  return fruit * 4;
+}
 
 function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  const applePieces = cutPieces(apples);
+  const orangePieces = cutPieces(oranges);
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
   return juice;
 }
+fruitProcessor(2, 3);

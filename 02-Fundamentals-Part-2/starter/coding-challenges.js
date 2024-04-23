5cfc6d7e4dd09803a487fa036db98226c7a2fc18
janagyjr <jnagyjr1978@gmail.com>
2024-04-23 14:06:08 -0500

    coding challenge 5 cont

--- 02-Fundamentals-Part-2/starter/coding-challenges.js
+++ 02-Fundamentals-Part-2/starter/coding-challenges.js
@@ -1,10 +1,10 @@
 // Part 2
 "use strict";
 const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
-  const scoreOne =
-  const scoreTwo =
-  const scoreThree = 
-}
+  const scoreDolpins = calcAverage(44, 23, 71);
+  const scoreKoalas = calcAverage(65, 54, 49);
+};
 
-
-
+console.log(
+  `The Koala's got ${scoreKoalas} and the Dolphins got ${scoreDolpins}`
+);
